e22dea66bc4de6a25537c9d1a067aeac43f318a7
Samuel <dev.19@pbhub.com.br>
2023-06-27 00:06:35 -0300

    feat: creater user route

--- api/src/routes/user/user.routes.ts
+++ api/src/routes/user/user.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { schemaValidation } from "../../middlewares/schemaValidation.middleware";
+import { registerUserSchema } from "../../schemas/user/registerUser.schema";
+import { createUserController } from "../../controllers/user/createUser.controller";
 
 const userApp = Router();
+
+userApp.post("/", schemaValidation(registerUserSchema), createUserController);
 
 export { userApp };
