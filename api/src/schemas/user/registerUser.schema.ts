2280ae60ba6627423ca7bc7dff2fb54fb4838de4
Samuel <dev.19@pbhub.com.br>
2023-06-26 23:52:24 -0300

    feat: user register schema

---
+++ api/src/schemas/user/registerUser.schema.ts
@@ -0,0 +1,33 @@
+import * as yup from "yup";
+
+interface IUserRequest {
+  name: string;
+  password: string;
+  photo_url?: string | null;
+  phone: string;
+  birth_date?: string | null;
+}
+
+const registerUserSchema: yup.Schema<IUserRequest> = yup.object().shape({
+  name: yup
+    .string()
+    .max(50, "o campo nome não pode ter mais de 50 caracteres")
+    .required("o campo nome é obrigatório"),
+  email: yup
+    .string()
+    .max(50, "o campo email não pode ter mais de 50 caracteres")
+    .email("o formato do e-mail é inválido")
+    .required("o campo nome é obrigatório"),
+  password: yup
+    .string()
+    .max(60, "o campo senha não pode ter mais de 60 caracteres")
+    .required("o campo senha é obrigatório"),
+  photo_url: yup.string().notRequired(),
+  phone: yup
+    .string()
+    .max(11, "o campo telefone não pode ter mais de 11 carcteres (DD996######")
+    .required("o campo telefone é obrigatório"),
+  birth_date: yup.string().notRequired(),
+});
+
+export { registerUserSchema };
