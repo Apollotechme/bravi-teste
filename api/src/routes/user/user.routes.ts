ce3024e4ab1813e0ab0c46593c98ee396cac9885
Samuel <dev.19@pbhub.com.br>
2023-06-27 00:40:18 -0300

    fix: user routes & prisma generate on dockerfile

--- api/src/routes/user/user.routes.ts
+++ api/src/routes/user/user.routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import { schemaValidation } from "../../middlewares/schemaValidation.middleware";
 import { registerUserSchema } from "../../schemas/user/registerUser.schema";
 import { createUserController } from "../../controllers/user/createUser.controller";
+import { ensureAuthMiddleware } from "../../middlewares/authUser.middleware";
+import { getOWnerUserController } from "../../controllers/user/getOwnerUser.controller";
 
 const userApp = Router();
 
 userApp.post("/", schemaValidation(registerUserSchema), createUserController);
+userApp.get("/", ensureAuthMiddleware, getOWnerUserController);
 
 export { userApp };
